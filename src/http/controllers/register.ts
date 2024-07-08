4c272cfb9fdb3b96939aa0b81610192f3e27af29
Mateus Ataide <teteulima@outlook.com>
2024-07-08 11:06:15 -0300

    feat/main: created use case error

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/registerUseCase'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -18,9 +19,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (error) {
-    if (error instanceof Error) {
+    if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message })
     }
+
+    return reply.status(500).send({ message: 'Internal server error' })
   }
 
   return reply.status(201).send()
