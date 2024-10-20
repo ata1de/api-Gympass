7ffcfbdfd4b666208d4b55992e3b92843ac0b98a
Seu Nome <seuemail@example.com>
2024-10-20 08:56:01 -0300

    reafactor: changed postgres database to sqlite

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,7 +5,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
   JWT_SECRET: z.string(),
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
